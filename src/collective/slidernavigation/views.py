82ec31adf6a20be9fba1b8e20eda0171d711e086
Maik Derstappen <maik.derstappen@inqbus.de>
2012-04-02 19:01:36 +0200

    add SliderNavigation viewlet and pane_content view, also use ajax to get the content from pane_content view

---
+++ src/collective/slidernavigation/views.py
@@ -0,0 +1,49 @@
+from zope.site.hooks import getSite
+from zope.publisher.browser import BrowserView
+
+import logging
+
+log = logging.getLogger("collective.slidernavigation.views:")
+
+
+class PaneContentView(BrowserView):
+    """ Return pane content as HTML and will be called by AJAX request.
+    """
+
+    def __call__(self):
+        """
+        """
+        self.pane_content = {}
+        self.pane_content = self.get_pane_content()
+        return self.index()
+
+
+    def get_pane_content(self):
+        """ Return pane content as dict.
+        """
+        portal = getSite()
+        portal_catalog = portal.portal_catalog
+        query = {}
+        uid = self.request.get("pane_content_uid")
+        if not uid:
+            log.debug("no uid given!")
+            return
+
+        query["UID"] = uid
+        results = portal_catalog(query)
+        if not results:
+            log.debug("no results for given uid '%s' !" % uid)
+            return
+
+        brain = results[0]
+        pane_content = {}
+        pane_content["id"] = brain.id
+        pane_content["title"] = brain.Title
+        pane_content["description"] = brain.Description
+        if brain.hasContentLeadImage:
+            pane_content["img_url"] = brain.getURL() + '/image_mini'
+        log.debug("pane content: %s" % pane_content)
+        return pane_content
+
+
+
