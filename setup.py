49bf4be4169d617311c034135a16087d803336cb
Maik Derstappen <maik.derstappen@inqbus.de>
2012-04-02 18:58:53 +0200

    add dependencies to collective.contentleadimage in setup.py and metadata.xml

--- setup.py
+++ setup.py
@@ -9,9 +9,7 @@ NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 version = '0.1'
 
 install_requires = [
-    # List your project dependencies here.
-    # For more details, see:
-    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+    'collective.contentleadimage',
 ]
 
 
