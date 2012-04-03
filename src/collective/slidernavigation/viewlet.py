390aa5d6eedcea3b164842cc1a1e2c76a3f01e03
Maik Derstappen <maik.derstappen@inqbus.de>
2012-04-03 19:04:20 +0200

    add properties to allow definition of different source folders for the slidernavigation viewlet, add property bottom_level and travers up to this level if current level is higher

--- src/collective/slidernavigation/viewlet.py
+++ src/collective/slidernavigation/viewlet.py
@@ -1,3 +1,5 @@
+from Acquisition import aq_inner, aq_parent
+from zope.component import getMultiAdapter
 from zope.site.hooks import getSite
 from plone.app.layout.viewlets.common import ViewletBase
 
@@ -10,14 +12,39 @@ class SliderNavigation(ViewletBase):
         """
         """
         self.portal = getSite()
-        self.current_url = self.portal.portal_url
+        self.base_url = self.portal.portal_url
         self.children = self.get_children()
 
     def get_children(self):
         """ return filtered navigation children of the curent folder
         """
         portal_catalog = self.portal.portal_catalog
-        path = '/'.join(self.context.getPhysicalPath())
+        plone_portal_state = getMultiAdapter((self.context, self.request),
+                name='plone_portal_state')
+        sproperties = self.portal.portal_properties.get("slidernavigation_properties", None)
+        if sproperties:
+            bottom_level = sproperties.getProperty("bottom_level", 0)
+            navigation_root_path = plone_portal_state.navigation_root_path()
+            navigation_root = navigation_root_path.split("/")
+            navigation_root_offset = len(navigation_root)
+            current_context = self.context
+            under_bottom_level = True
+            while under_bottom_level:
+                current_path = '/'.join(current_context.getPhysicalPath())
+                current_level = len(current_path.split("/")) - navigation_root_offset
+                if current_level <= bottom_level:
+                    under_bottom_level = False
+                else:
+                    current_context = aq_parent(aq_inner(current_context))
+            path = current_path
+        else:
+            current_path = '/'.join(self.context.getPhysicalPath())
+
+        slidernavigation_source_path = self.context.getProperty("slidernavigation_source_path", None)
+        if slidernavigation_source_path:
+            path = slidernavigation_source_path
+        else:
+            path = current_path
         query={
             'path': {'query': path},
             'portal_type': 'Folder',
@@ -25,6 +52,6 @@ class SliderNavigation(ViewletBase):
         }
         results = portal_catalog(query)
         children = [child for child in results
-                if not child.exclude_from_nav and not child.id == self.context.id]
+                if not child.exclude_from_nav and not child.getPath() == path]
         return children
 
