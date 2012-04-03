e535a1130918adb7de7cc45e5d25ddebe2c05cc4
Maik Derstappen <maik.derstappen@inqbus.de>
2012-04-03 15:35:31 +0200

    add custom ajax method to load pane content more specific and allow separate effects and use rel to get the load url

--- src/collective/slidernavigation/slidernavigation.js
+++ src/collective/slidernavigation/slidernavigation.js
@@ -1,6 +1,26 @@
 jq(document).ready(function() {
   jq("ul#slidernav-tabs").tabs("#slidernav-panes > div", {
-    effect: 'ajax',
+    effect: 'fade',
     event: 'mouseover',
+    rotate: true,
   });
+
+  var api = jq("#slidernav-tabs").data("tabs");
+
+  function load_pane_content(event, index) {
+    // get the pane to be opened
+    if(index == undefined){
+      var index = api.getIndex();
+    }
+    console.log(index);
+    var pane = api.getPanes().eq(index);
+    api.click(index);
+    if (!pane.hasClass("loaded")) {
+      pane.load(api.getTabs().eq(index).attr("rel")).addClass("loaded");
+    }
+  }
+  
+  load_pane_content(0, 2);
+  jq("ul#slidernav-tabs").bind("mouseover load", load_pane_content);
+   
 });
